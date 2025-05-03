f23ab51c8be4c7844a25bebd63511049f55211ec
@IT20036270 <it20036270@my.sliit.lk>
2025-05-03 12:10:15 +0530

    gitignore check

--- enchanted_elegance/src/main/java/com/enchantedelegance/root/RootPath.java
+++
@@ -1,8 +0,0 @@
-package com.enchantedelegance.root;
-
-public class RootPath {
-
-    //data txt
-    public static String rootPath = "C:/Users/Harsha/Desktop/Java/enchanted_elegance";
-    // public static String rootPath = "";
-}
