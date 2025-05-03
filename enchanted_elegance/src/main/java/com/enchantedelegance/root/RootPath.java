5b8deb67a0598389308c24fe984ee65895a37164
Harsha Prabhath <70996296+Harshaprabath@users.noreply.github.com>
2025-05-03 15:42:37 +0530

    Update RootPath.java

--- enchanted_elegance/src/main/java/com/enchantedelegance/root/RootPath.java
+++ enchanted_elegance/src/main/java/com/enchantedelegance/root/RootPath.java
@@ -1,7 +1,7 @@
 package com.enchantedelegance.root;
 
 public class RootPath {
-    //data txt root files path
+    
     public static String rootPath = "C:/Users/Harsha/Desktop/Java/enchanted_elegance";
     // public static String rootPath = "";
 }
